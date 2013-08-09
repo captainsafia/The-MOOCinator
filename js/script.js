5d671e705c1ddbdcb02dd0d71f6b5f5d808122aa
Safia Abdalla <safia.lambda@gmail.com>
2013-08-09 15:28:50 +0300

    Changed interface

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$(window).resize(function(){
+
+		$('.well').css({
+			position:'absolute',
+			left: ($(window).width() - $('.well').outerWidth())/2,
+			top: ($(window).height() - $('.well').outerHeight())/2
+		});
+
+	});
+
+// To initially run the function:
+$(window).resize();
+});
